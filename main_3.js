8c28b3230e361974ba38ff465728164103b9186b
Mykyta Nekh <nikita.nekh@gmail.com>
2021-04-13 22:55:43 +0300

    Update main_3.js

--- main_3.js
+++ main_3.js
@@ -3,16 +3,17 @@ const randomIntegerFromInterval = (min, max) => {
 };
 
 const makeTransaction = (transaction) => {
-    const delay = randomIntegerFromInterval(200, 500);
-    console.log(delay)
+    // console.log(delay)
+      const delay = randomIntegerFromInterval(200, 500);
+
     return new Promise((resolve, reject) => {
          setTimeout(() => {
     const canProcess = Math.random() > 0.3;
 
     if (canProcess) {
-      resolve(`Transaction ${transaction.id} processed in ${delay}ms`);
+      resolve([transaction.id, delay]);
     } else {
-      reject(`Error processing transaction ${transaction.id}. Please try again later.`);
+      reject(transaction.id);
     }
   }, delay);
     })
@@ -20,12 +21,12 @@ const makeTransaction = (transaction) => {
  
 };
 
-const logSuccess = (result) => {
-  console.log(result);
+const logSuccess = ([id, time]) => {
+  console.log(`Transaction ${id} processed in ${time}ms`);
 };
 
-const logError = result => {
-  console.warn(result);
+const logError = id => {
+  console.warn(`Error processing transaction ${id}. Please try again later.`);
 };
 
 /*
